24e06e56f85f3f0a1efcfdf61c3a28faa8dcef8e
Akhmad <akhmad@Akhmads-MacBook-Pro.local>
2023-08-19 12:32:24 +0600

    Logistic Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import "./components/styles/index.css";
+import Services from "./components/services/Services";
+import About from "./components/about/About";
+import WhyUs from "./components/whyUs/WhyUs";
+import Contact from "./components/contact/Contact";
+import Partnership from "./components/partnership/Partnership";
+import { AllAppContextProvider } from "./context/AllAppContext";
+
+// Creating Layout for children pages
+const Layout = () => {
+  return (
+    <AllAppContextProvider>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </AllAppContextProvider>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      ,
+      {
+        path: "/partnership",
+        element: <Partnership />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/whyUs",
+        element: <WhyUs />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    </main>
   );
 }
 
